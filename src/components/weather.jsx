a0fa82de6fddd32a7f440bb44dbfc1c698650adb
Mitchell Perry <mitchell.perry@simplybusiness.co.uk>
2025-01-22 16:35:20 +0000

    add weather jsx and css files

---
+++ src/components/weather.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './weather.css';
+
+export const Weather = () => {
+  return (
+    <div className='weather'>
+      <div className='weather-container'>
+        <div className='search-bar'>
+          <input type='text' placeholder='Enter city name'/>
+          <button>Search</button>
+        </div>
+        <p className='city'>London</p>
+        <p className='temperature'>20°C</p>
+        <div className='weather-data'>
+          <div className='col'>
+            <div>
+              <p>Humidity</p>
+              <p>20%</p>
+            </div>
+          </div>
+          <div className='col'>
+            <div>
+              <p>Wind Speed</p>
+              <p>50 kmh</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Weather;
