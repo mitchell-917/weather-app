e429e123b081631111c717e33908f687e60d10f5
Mitchell Perry <mitchell.perry@simplybusiness.co.uk>
2025-01-29 14:46:32 +0000

    refactor test for improved readability and add describe block

--- src/App.test.js
+++ src/App.test.js
@@ -2,63 +2,53 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import Weather from './components/weather.jsx';
 
-test('renders weather app UI elements', () => {
-  render(<App/>);
+describe('Weather App', () => {
+  test('renders weather app UI elements', () => {
+    render(<App />);
 
-  const cityElement = screen.getByText('London');
-  expect(cityElement).toBeInTheDocument();
+    expect(screen.getByText('London')).toBeInTheDocument();
+    expect(screen.getByText('20°C')).toBeInTheDocument();
+    expect(screen.getByText('Humidity')).toBeInTheDocument();
+    expect(screen.getByText('Wind Speed')).toBeInTheDocument();
+    expect(screen.getByText('20%')).toBeInTheDocument();
+    expect(screen.getByText('50 kmh')).toBeInTheDocument();
+    expect(screen.getByTestId('current-weather')).toBeInTheDocument();
+  });
 
-  const temperatureElement = screen.getByText('20°C');
-  expect(temperatureElement).toBeInTheDocument();
+  it('renders sunny weather image', () => {
+    const currentWeather = "Clear";
+    render(<Weather weather={currentWeather}/>);
 
-  const humidityElement = screen.getByText('Humidity');
-  expect(humidityElement).toBeInTheDocument();
+    const currentWeatherImage = screen.getByTestId('current-weather');
+    expect(currentWeatherImage).toBeInTheDocument();
+    expect(currentWeatherImage.getAttribute('src')).toBe('/assets/clear.png');
+  });
 
-  const windSpeedElement = screen.getByText('Wind Speed');
-  expect(windSpeedElement).toBeInTheDocument();
+  xit('renders cloudy weather image', () => {
+    const currentWeather = "Cloudy";
+    render(<Weather weather={currentWeather}/>);
 
-  const humidityElementValue = screen.getByText('20%');
-  expect(humidityElementValue).toBeInTheDocument();
+    const currentWeatherImage = screen.getByTestId('current-weather');
+    expect(currentWeatherImage).toBeInTheDocument();
+    expect(currentWeatherImage.getAttribute('src')).toBe('/assets/cloud.png');
+  });
 
-  const windSpeedElementValue = screen.getByText('50 kmh');
-  expect(windSpeedElementValue).toBeInTheDocument();
+  it('renders rainy weather image', () => {
+    const currentWeather = "Rainy";
+    render(<Weather weather={currentWeather}/>);
 
-  const currentWeatherElement = screen.getByTestId('current-weather');
-  expect(currentWeatherElement).toBeInTheDocument();
+    const currentWeatherImage = screen.getByTestId('current-weather');
+    expect(currentWeatherImage).toBeInTheDocument();
+    expect(currentWeatherImage.getAttribute('src')).toBe('/assets/rain.png');
+  });
+
+  it('renders snowy weather image', () => {
+    const currentWeather = "Snowy";
+    render(<Weather weather={currentWeather}/>);
+
+    const currentWeatherImage = screen.getByTestId('current-weather');
+    expect(currentWeatherImage).toBeInTheDocument();
+    expect(currentWeatherImage.getAttribute('src')).toBe('/assets/snow.png');
+  });
 });
 
-it('renders sunny weather image', () => {
-  const currentWeather = "Clear";
-  render(<Weather weather={currentWeather}/>);
-
-  const currentWeatherImage = screen.getByTestId('current-weather');
-  expect(currentWeatherImage).toBeInTheDocument();
-  expect(currentWeatherImage.getAttribute('src')).toBe('/assets/clear.png');
-});
-
-xit('renders cloudy weather image', () => {
-  const currentWeather = "Cloudy";
-  render(<Weather weather={currentWeather}/>);
-
-  const currentWeatherImage = screen.getByTestId('current-weather');
-  expect(currentWeatherImage).toBeInTheDocument();
-  expect(currentWeatherImage.getAttribute('src')).toBe('/assets/cloud.png');
-});
-
-it('renders rainy weather image', () => {
-  const currentWeather = "Rainy";
-  render(<Weather weather={currentWeather}/>);
-
-  const currentWeatherImage = screen.getByTestId('current-weather');
-  expect(currentWeatherImage).toBeInTheDocument();
-  expect(currentWeatherImage.getAttribute('src')).toBe('/assets/rain.png');
-});
-
-it('renders snowy weather image', () => {
-  const currentWeather = "Snowy";
-  render(<Weather weather={currentWeather}/>);
-
-  const currentWeatherImage = screen.getByTestId('current-weather');
-  expect(currentWeatherImage).toBeInTheDocument();
-  expect(currentWeatherImage.getAttribute('src')).toBe('/assets/snow.png');
-});
