2337e3a8b2ed2abcac53e5a439654bc984331af1
Mitchell Perry <mitchell.perry@simplybusiness.co.uk>
2025-04-02 14:00:40 +0100

    setup api call with the correct way to get keys

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -4,8 +4,29 @@ import Search from './search';
 import Humidity from './humidity';
 import Wind from './wind';
 import CurrentWeather from './currentWeather';
+import { useEffect } from 'react';
 
 export const Weather = () => {
+  const search = async (city) => {
+    try {
+        const apiKey = process.env.REACT_APP_API_KEY;
+        console.log(process.env);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const city = 'London'
+      await search(city);
+    };
+    fetchWeather();
+  }, []);
+
   return (
     <div className='weather'>
       <div className='weather-container'>
