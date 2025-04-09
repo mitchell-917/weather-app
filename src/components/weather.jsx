c158ccd58d09d8437153e6f817651aff031489fe
Mitchell Perry <mitchell.perry@simplybusiness.co.uk>
2025-04-09 13:34:14 +0100

    add weather data

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -7,11 +7,14 @@ import CurrentWeather from './currentWeather';
 import { useEffect } from 'react';
 
 export const Weather = () => {
+  const WeatherData = 20;
+
   const search = async (city) => {
     try {
         const apiKey = process.env.REACT_APP_API_KEY;
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
         const data = await response.json();
+
     } catch (error) {
         console.error('Error fetching weather data:', error);
     }
@@ -29,7 +32,7 @@ export const Weather = () => {
     <div className='weather'>
       <div className='weather-container'>
         <Search />
-        <CurrentWeather />
+        <CurrentWeather temperature={WeatherData || 20}/>
         <div className='weather-data'>
           <div className='col'>
             <Humidity />
