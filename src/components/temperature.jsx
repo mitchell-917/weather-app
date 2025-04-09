c388d428325fe8540a0cb857e45d49a91a7996ea
Mitchell Perry <mitchell.perry@simplybusiness.co.uk>
2025-04-09 13:15:01 +0100

    default set value for temperature

--- src/components/temperature.jsx
+++ src/components/temperature.jsx
@@ -1,12 +1,14 @@
 
-export const Temperature = () => {
-  var temperature = 20;
-
+export const Temperature = ({temperature}) => {
   return (
     <div className='temperature'>
       <p>{temperature}°</p>
     </div>
   );
 }
+
+Temperature.defaultProps = {
+  temperature: 20,
+};
 
 export default Temperature;
